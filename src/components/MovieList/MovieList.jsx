6cd29f26da99dea66933dd2b8af9e519cc8d7147
ihorborys <ihorborys@gmail.com>
2025-05-24 18:35:27 +0300

    Edited images mobile responsiveness

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -24,7 +24,7 @@ const MovieList = ({ movies }) => {
               height={278}
               alt="Movie poster image"
             />
-            <p className={styles.title}>{movie.original_title}</p>
+            <div className={styles.title}>{movie.original_title}</div>
           </Link>
         </li>
       ))}
